092c47abf1abd23f134849fd544420947408227e
Allen Sarmiento <42989060+Allen-Sarmiento@users.noreply.github.com>
2018-09-25 19:17:17 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+
+int main() {
+	/*
+	std::cout << "Azeem's Program\n";
+	double state = 0.04;
+	double county = 0.02;
+
+	std::cout << (95 * state) + (95 * county) << std::endl;
+
+	std::cout << "Jamie's Program\n";
+	double st = 0.04;
+	double ct = 0.02;
+	std::cout << "Calculation for the state tax: " << st * 95 << '\n';
+	std::cout << "Calculation for the county tax: " << ct * 95 << '\n';
+	double total1 = st * 95;
+	double total2 = ct * 95;
+	double total3 = total1 + total2;
+	std::cout << total3 << '\n';
+	*/
+
+	/*
+	int tank = 20;
+	double highw = 29.9;
+	double town = 23.5;
+
+	std::cout << "With a full tank the car can travel " << (tank * highw) << " miles on a highway\n";
+	std::cout << "With a full tank the car can travel " << (tank * town) << " miles on a highway\n";
+	*/
+
+	/*
+	double question1, question2, question3, question4;
+	question1 = 10.0 + 4.0 * 3.0 - 2.0;
+	question2 = 10.0 / (4.0 + 3.0) / 2.0;
+	question3 = 8.0 + 2 % 3 * 1.0;
+	question4 = 1.0 + 3.0 * 2 / 2 % 1;
+
+	std::cout << "Question 1: " << question1 << '\n';
+	std::cout << "Question 2: " << question2 << '\n';
+	std::cout << "Question 3: " << question3 << '\n';
+	std::cout << "Question 4: " << question4 << '\n';
+	*/
+
+	/*
+	double x = 2.5555555;
+	std::cout << x << '\n';
+	*/
+
+	/*
+	double hours, minutes;
+
+	// Write code here that prompts the user
+	// to enter how many hours they studied over the weekend
+	// and reads the input into the hours variable.
+	std::cout << "How many hours did you study over the weekend?\n";
+	std::cin >> hours;
+
+	// The following line does the conversion.
+	minutes = hours * 60;
+
+	// Write code here that displays the amount of
+	// minutes spent studying.
+	std::cout << "Minutes studied: " << minutes << std::endl;
+	*/
+
+	return 0;
+}
